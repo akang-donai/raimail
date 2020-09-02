81c6dad1074d270d4ef59033dcdacc42594d946c
akang-donai <40636630+akang-donai@users.noreply.github.com>
2020-09-02 11:40:57 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ## iRedmail Armbian ##
-cd /etc/apt/ 
-wget https://raw.githubusercontent.com/akang-donai/raimail/master/sources.list
-apt update && apt upgrade -y
-sed -i 's/localhost tinkerboard/usagi.mizutech.id localhost/g' /etc/hosts
-sed -i 's/tinkerboard/usagi.mizutech.id/g' /etc/hostname
-echo "Asia/Jakarta" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
+cd /etc/apt/ && mv sources.list sources.list.bak && \
+wget https://raw.githubusercontent.com/akang-donai/raimail/master/sources.list && \
+apt update && apt upgrade -y && \
+sed -i 's/localhost tinkerboard/usagi.mizutech.id localhost/g' /etc/hosts && \
+sed -i 's/tinkerboard/usagi.mizutech.id/g' /etc/hostname && \
+echo "Asia/Jakarta" > /etc/timezone && \
+ln -fs /usr/share/zoneinfo/`cat /etc/timezone` /etc/localtime && \
+dpkg-reconfigure -f noninteractive tzdata && \
 reboot
